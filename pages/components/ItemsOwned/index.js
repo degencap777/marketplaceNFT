7049fc900f194abdc94a8f373ccfdc6b1f8000c3
libraiger <misssilverbeauty0927@outlook.com>
2022-11-12 13:27:48 -0600

    blackdark defined

--- pages/components/ItemsOwned/index.js
+++ pages/components/ItemsOwned/index.js
@@ -1,6 +1,8 @@
 export default function ItemsOwned() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col py-10">
+      <h1 className="text-4xl">ITEMS OWNED</h1>
+      
     </div>
   )
 }
