39fe6c00e4a3271b63a243aa5f3986714e467035
libraiger <misssilverbeauty0927@outlook.com>
2022-11-12 13:33:29 -0600

    NFTDetail datalist add

---
+++ pages/components/NFTDetail/dataList.js
@@ -0,0 +1,21 @@
+import NFT1 from "../../../public/nft1.png";
+import NFT2 from "../../../public/nft2.png";
+
+export const NFTDetailList = [
+  {
+    id: 1,
+    src: NFT1,
+    title: "CENTRAL CEE",
+    fee: "0.0433%",
+    bundle: "1 BASIC BUNDLES",
+    spent: "0.3 ETHER",
+  },
+  {
+    id: 2,
+    src: NFT2,
+    title: "SHALO",
+    fee: "0.4%",
+    bundle: "2 LUXURY BUNDLES",
+    spent: "0.04 ETHER",
+  },
+]
