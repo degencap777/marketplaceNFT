e38370798e81500574495097b7c8a5218c50778f
libraiger <misssilverbeauty0927@outlook.com>
2022-11-12 20:31:21 -0600

    medium font

--- pages/components/NFTDetail/index.js
+++ pages/components/NFTDetail/index.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function NFTDetail({ NFT }) {
   return (
-    <div className="bg-app-black-dark flex px-2 py-1 items-center" style={{fontFamily:'Furore'}}>
+    <div className="bg-app-black-dark flex px-3 py-2 items-center" style={{fontFamily:'Furore'}}>
       <Image src={NFT.src} alt={NFT.title} title={NFT.title} width={200} height={200} />
-      <div className="flex flex-col gap-4 mx-3 w-1/3">
+      <div className="flex flex-col gap-4 mx-3 w-5/12">
         <h1 className="text-5xl font-medium" >{NFT.title}</h1>
         <h2 className="text-3xl text-app-orange font-bold">OWNED</h2>
         <h2 className="text-2xl font-bold">{NFT.fee}</h2>
