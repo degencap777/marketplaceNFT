6fe87dfbe7fd06979781f75dbace6bb938bdb40e
libraiger <misssilverbeauty0927@outlook.com>
2022-11-12 19:42:42 -0600

    Complete Item-index

--- pages/components/NFTDetail/index.js
+++ pages/components/NFTDetail/index.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function NFTDetail({ NFT }) {
   return (
-    <div className="bg-app-black-dark flex px-2 py-1">
-      <Image src={NFT.src} alt={NFT.title} title={NFT.title} />
+    <div className="bg-app-black-dark flex px-2 py-1 items-center">
+      <Image src={NFT.src} alt={NFT.title} title={NFT.title} width={200} height={200} />
       <div className="flex flex-col gap-4 mx-3">
-        <h1 className="tet-3xl">{NFT.title}</h1>
-        <h2 className="text-xl text-app-orange">OWNED</h2>
-        <h2 className="text-xl">{NFT.fee}</h2>
-        <h2 className="text-xl">{NFT.bundle}</h2>
+        <h1 className="text-4xl font-bold">{NFT.title}</h1>
+        <h2 className="text-2xl text-app-orange font-bold">OWNED</h2>
+        <h2 className="text-2xl font-bold">{NFT.fee}</h2>
+        <h2 className="text-2xl font-bold">{NFT.bundle}</h2>
       </div>
       <div className="flex mx-5">
         <svg width="4" height="170" viewBox="0 0 4 170" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -16,8 +16,8 @@ export default function NFTDetail({ NFT }) {
         </svg>
       </div>
       <div className="flex flex-col justify-center">
-        <h3 className="text-lg">SPENT</h3>
-        <h2 className="text-app-orange">{NFT.spent}</h2>
+        <h3 className="text-xl font-medium">SPENT</h3>
+        <h2 className="text-app-orange text-2xl font-bold">{NFT.spent}</h2>
       </div>
     </div>
   )
