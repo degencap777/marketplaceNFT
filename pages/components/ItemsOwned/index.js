cc410975547cf07a2977f26e8cdd32254ff067ca
libraiger <misssilverbeauty0927@outlook.com>
2022-11-12 19:42:25 -0600

    ItemsOwned before mobile responsive

--- pages/components/ItemsOwned/index.js
+++ pages/components/ItemsOwned/index.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import { NFTDetailList } from "./dataList"
 import NFTDetail from "../NFTDetail"
 
 export default function ItemsOwned() {
+  console.log(NFTDetailList);
   return (
-    <div className="flex flex-col py-10">
-      <h1 className="text-4xl">ITEMS OWNED</h1>
-      <div>
-        {NFTDetailList.map((idx, menu) => {
+    <div className="flex flex-col py-5">
+      <h1 className="text-4xl font-bold py-8">ITEMS OWNED</h1>
+      <div className="flex flex-col gap-8 w-3/4">
+        {NFTDetailList.map((menu, idx) => {
           console.log(menu);
           return (
             <NFTDetail NFT={menu} key={idx} />
-          )
+          );
         }
-
-
         )}
-
       </div>
     </div>
-
   )
 }
